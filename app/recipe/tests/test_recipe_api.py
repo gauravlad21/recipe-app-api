68422f31ab6748b6859e2f9ac22b41e960fa634a
gauravlad2 <gauravkumar.lad@nirman.io>
2022-12-18 01:33:34 +0530

    added Recipe-details

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,9 +9,16 @@ from rest_framework.test import APIClient
 
 from core.models import Recipe
 
-from recipe.serializer import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+)
 
 RECIPE_URL = reverse("recipe:recipe-list")
+
+
+def details_url(recipe_id):
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def create_recipe(user, **params):
@@ -73,3 +80,13 @@ class PrivateRecipeTests(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        recipe = create_recipe(user=self.user)
+
+        url = details_url(recipe_id=recipe.id)
+        res = self.client.get(url)
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)
+
+    # def test_create_recipe(self):
