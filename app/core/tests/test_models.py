d9c3f9112ddf5173fe6f31ccd150fd54a748dd36
gauravlad2 <gauravkumar.lad@nirman.io>
2022-12-18 01:10:55 +0530

    added Recipe view and serializers

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -46,7 +46,7 @@ class ModelTests(TestCase):
         recipe = models.Recipe.objects.create(
             user=user,
             title='Sample recipe name',
-            time_minutes=5,
+            time_minute=5,
             price=Decimal('5.50'),
             description='Sample recipe description',
         )
